98a7c1e15879b31374ee5a45e52534af52e339f4
RdotSilva <RyandotSilva@gmail.com>
2020-10-17 20:21:18 -0400

    Add ShoppingDetailsComponent

---
+++ src/app/shopping-list/shopping-details/shopping-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-details.component.html',
+  styleUrls: ['./shopping-details.component.css'],
+})
+export class ShoppingDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

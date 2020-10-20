b2f0aef3564c4df419e396f4039a790a8b94c379
RdotSilva <RyandotSilva@gmail.com>
2020-10-20 19:26:22 -0400

    Add additional test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,6 +15,11 @@ export class RecipeListComponent implements OnInit {
       'This is only a test',
       'https://static2.clutch.co/s3fs-public/logos/recipe_advertising_logo.png'
     ),
+    new Recipe(
+      'Test Recipe Two',
+      'This is another test',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
   ];
 
   constructor() {}

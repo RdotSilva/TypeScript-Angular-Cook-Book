8ba8c9efc015340a58062039135ca55a3be17b9e
RdotSilva <RyandotSilva@gmail.com>
2020-10-22 20:25:37 -0400

    Update hard coded ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,8 +8,8 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient('Apples', 5, 'Produce', 4),
+    new Ingredient('Tomatoes', 10, 'Produce', 10),
   ];
 
   constructor() {}

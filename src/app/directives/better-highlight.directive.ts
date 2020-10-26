b850613f91c64a574e050beeb0120eb9ac40c4fd
RdotSilva <RyandotSilva@gmail.com>
2020-10-26 19:22:56 -0400

    Add mouseOver host listener

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -7,10 +13,19 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'white',
+    //   false,
+    // );
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'background-color',
-      'orange'
+      'blue'
     );
   }
 }

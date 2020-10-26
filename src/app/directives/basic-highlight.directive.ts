ac9d5b6f074b89cc57fec4ee8503bbc140e19e00
RdotSilva <RyandotSilva@gmail.com>
2020-10-26 18:57:46 -0400

    Add ngOnInit method

--- src/app/directives/basic-highlight.directive.ts
+++ src/app/directives/basic-highlight.directive.ts
@@ -5,4 +5,8 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 })
 export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.elementRef.nativeElement.style.backgroundColor = 'white';
+  }
 }

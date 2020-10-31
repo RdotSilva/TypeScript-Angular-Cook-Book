8c27c8b6e96d7c43182003cbf9e5680713bb16ea
RdotSilva <RyandotSilva@gmail.com>
2020-10-30 20:30:29 -0400

    Add toggleOpen directive

--- src/app/shared/dropdown-outside-click.directive.ts
+++ src/app/shared/dropdown-outside-click.directive.ts
@@ -10,6 +10,10 @@ import {
 })
 export class DropdownOutsideDirective {
   @HostBinding('class.open') isOpen = false;
-
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
+  }
   constructor(private elRef: ElementRef) {}
 }

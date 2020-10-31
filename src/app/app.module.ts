96b69e28a37f6640dfbb37c800f36acb8684709c
RdotSilva <RyandotSilva@gmail.com>
2020-10-30 20:28:03 -0400

    Add DropdownOutsideDirective declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { BetterHighlightDirective } from './directives/better-highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { DropdownOutsideDirective } from './shared/dropdown-outside-click.directive';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    DropdownOutsideDirective,
     FavoritesComponent,
     BetterHighlightDirective,
     UnlessDirective,

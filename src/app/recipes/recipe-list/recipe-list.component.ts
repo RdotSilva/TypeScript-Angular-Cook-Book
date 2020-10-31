93045fddf4a7d9b857080109d8fae5188ab6090c
RdotSilva <RyandotSilva@gmail.com>
2020-10-30 20:48:07 -0400

    Add providers

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,7 @@ import { LoggingService } from './../../shared/service/logging.service';
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
+  providers: [LoggingService],
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();

7a8b9b8467fdd54f03f215fc75e536bbf380768a
RdotSilva <RyandotSilva@gmail.com>
2020-11-02 20:16:00 -0500

    Add RecipesService to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { UnlessDirective } from './directives/unless.directive';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DropdownOutsideDirective } from './shared/dropdown-outside-click.directive';
+import { RecipesService } from './shared/service/recipes.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { DropdownOutsideDirective } from './shared/dropdown-outside-click.direct
     UnlessDirective,
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [RecipesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

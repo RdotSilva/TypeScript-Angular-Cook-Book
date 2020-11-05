e4d9e27910b9fb920b5d631308277bba3df13656
RdotSilva <RyandotSilva@gmail.com>
2020-11-04 21:49:59 -0500

    Add getRecipes method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,4 +13,8 @@ export class RecipeService {
       'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
     ),
   ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }

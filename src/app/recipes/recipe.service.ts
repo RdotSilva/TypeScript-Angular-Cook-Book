47924941348a527a29ddced419e14960f41d90c1
RdotSilva <RyandotSilva@gmail.com>
2020-11-04 21:49:17 -0500

    Add RecipeService class

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is only a test',
+      'https://static2.clutch.co/s3fs-public/logos/recipe_advertising_logo.png'
+    ),
+    new Recipe(
+      'Test Recipe Two',
+      'This is another test',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+  ];
+}

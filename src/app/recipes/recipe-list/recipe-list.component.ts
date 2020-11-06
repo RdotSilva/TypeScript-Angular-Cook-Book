0cdd87ac2707c7cd13da4ae0552e80b27e41c2ba
RdotSilva <RyandotSilva@gmail.com>
2020-11-05 19:48:29 -0500

    Clean up component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,13 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { LoggingService } from './../../shared/service/logging.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
-  providers: [LoggingService],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
   recipes: Recipe[] = [];
 
   constructor(private recipeService: RecipeService) {}

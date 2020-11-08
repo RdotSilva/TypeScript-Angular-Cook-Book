4867e032aa460794b144a422341b97341e8ed8c4
RdotSilva <RyandotSilva@gmail.com>
2020-11-07 20:03:31 -0500

    Add ShoppingListService class

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,8 @@
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5, 'Produce', 4, 'Yes'),
+    new Ingredient('Tomatoes', 10, 'Produce', 10, 'No'),
+  ];
+}

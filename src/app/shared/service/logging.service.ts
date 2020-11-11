5baabe295beea6d26eebddc253463fef2987d37c
RdotSilva <RyandotSilva@gmail.com>
2020-11-10 20:14:29 -0500

    Add logIngredient method

--- src/app/shared/service/logging.service.ts
+++ src/app/shared/service/logging.service.ts
@@ -2,4 +2,8 @@ export class LoggingService {
   logRecipe(recipe: string) {
     console.log('New recipe added: ' + recipe);
   }
+
+  logIngredient(ingredient: string) {
+    console.log('New ingredient added: ' + ingredient);
+  }
 }

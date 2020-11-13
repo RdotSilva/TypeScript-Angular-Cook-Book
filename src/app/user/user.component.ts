0e56494abcd9cf1ffe6cb7df48bd732789eccca3
RdotSilva <RyandotSilva@gmail.com>
2020-11-13 18:52:25 -0500

    Add user property

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  constructor() {}
+  user: { id: number; name: string };
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
 }

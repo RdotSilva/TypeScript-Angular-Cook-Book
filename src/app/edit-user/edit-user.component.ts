c1650bfb8bfb09393f3355f83695be99bb1ca5f4
RdotSilva <RyandotSilva@gmail.com>
2020-11-16 18:35:24 -0500

    Update ngOnInit to show query params

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
@@ -9,7 +10,10 @@ export class EditUserComponent implements OnInit {
   id: number;
   name: string;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+  }
 }

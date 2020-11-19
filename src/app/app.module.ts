c29aeda10c7e5efc9c5546143dd9e1c045ef5da8
RdotSilva <RyandotSilva@gmail.com>
2020-11-18 19:26:49 -0500

    Clean up module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,53 +14,10 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DropdownOutsideDirective } from './shared/dropdown-outside-click.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RouterModule, Routes } from '@angular/router';
-import { IngredientsComponent } from './ingredients/ingredients.component';
 import { UserComponent } from './user/user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { FavoriteUserComponent } from './favorite-user/favorite-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  {
-    path: 'favorites',
-    component: FavoritesComponent,
-  },
-  {
-    path: 'ingredients',
-    component: IngredientsComponent,
-  },
-  {
-    path: '',
-    component: RecipesComponent,
-  },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-  },
-  {
-    path: 'shoppinglist',
-    component: ShoppingListComponent,
-  },
-  {
-    path: 'user/:id/:name',
-    component: UserComponent,
-  },
-  {
-    path: 'user/:id/edit',
-    component: EditUserComponent,
-  },
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent,
-  },
-  {
-    path: '**',
-    redirectTo: '/not-found',
-  },
-  //TODO: Add nested child paths
-];
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -79,7 +36,7 @@ const appRoutes: Routes = [
     FavoriteUserComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })

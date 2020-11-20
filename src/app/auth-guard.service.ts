dc9cf26082c5f7900173265fc4368c58e2ddfeb6
RdotSilva <RyandotSilva@gmail.com>
2020-11-19 19:57:41 -0500

    Add AuthGuardService class

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,8 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+export class AuthGuardService implements CanActivate {}

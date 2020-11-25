db0e8a668e76344e7e882b877cf504eb2d8f3e03
RdotSilva <RyandotSilva@gmail.com>
2020-11-24 19:25:43 -0500

    Clean up configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,11 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { FormsModule } from '@angular/forms';
-import { FavoritesComponent } from './favorites/favorites.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { DropdownOutsideDirective } from './shared/dropdown-outside-click.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { UserComponent } from './user/user.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthService } from './auth.service';
-import { AuthGuardService } from './auth-guard.service';
-import { CanDeactivateGuard } from './can-deactivate-guard.service';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { FavoritesResolver } from './favorites/favorites-resolver.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,19 +27,10 @@ import { FavoritesResolver } from './favorites/favorites-resolver.service';
     ShoppingEditComponent,
     DropdownDirective,
     DropdownOutsideDirective,
-    FavoritesComponent,
     UserComponent,
-    PageNotFoundComponent,
-    ErrorPageComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [
-    ShoppingListService,
-    AuthService,
-    AuthGuardService,
-    CanDeactivateGuard,
-    FavoritesResolver,
-  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

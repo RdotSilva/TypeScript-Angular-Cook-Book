01046e55ba35419fb38ca94fb8b279603664cb34
RdotSilva <RyandotSilva@gmail.com>
2020-11-24 19:33:22 -0500

    Remove unused files

--- src/app/shared/service/logging.service.ts
+++
@@ -1,9 +0,0 @@
-export class LoggingService {
-  logRecipe(recipe: string) {
-    console.log('New recipe added: ' + recipe);
-  }
-
-  logIngredient(ingredient: string) {
-    console.log('New ingredient added: ' + ingredient);
-  }
-}

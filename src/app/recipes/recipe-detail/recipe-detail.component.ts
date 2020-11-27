33a6e19afc0cd8a7408e4a5109cc0513dddfa2af
RdotSilva <RyandotSilva@gmail.com>
2020-11-26 19:40:50 -0500

    Add logic to set recipe based on params

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -20,6 +20,7 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
     });
   }
 

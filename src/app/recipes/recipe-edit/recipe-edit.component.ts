384411f920cf1c6dcdb8b805d5d0285ab257c00b
RdotSilva <RyandotSilva@gmail.com>
2020-11-28 20:50:47 -0500

    Update ngOnInit to subscribe to params

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -12,5 +12,10 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
+  }
 }

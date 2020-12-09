e828e0132a6bf09ce5b5b398e64547c04b96f7ef
RdotSilva <RyandotSilva@gmail.com>
2020-12-08 21:04:50 -0500

    Clean up UserFormComponent class

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,43 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent {
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: '',
-  };
-  submitted = false;
-
-  @ViewChild('f') signupForm: NgForm;
-
-  suggestUserName(): void {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-      },
-    });
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
-  }
 }

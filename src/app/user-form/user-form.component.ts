0e0f1ee78461d5efeebb67e66aa84b07662ecc11
RdotSilva <RyandotSilva@gmail.com>
2020-12-10 20:30:06 -0500

    Add onAddHobby method

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './user-form.component.html',
@@ -16,10 +16,16 @@ export class UserFormComponent implements OnInit {
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 }

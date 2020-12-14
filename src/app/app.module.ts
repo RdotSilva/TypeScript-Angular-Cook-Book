5658a5399bc73a1b5acd8f00067c6ebd9f0d8366
RdotSilva <RyandotSilva@gmail.com>
2020-12-13 20:10:37 -0500

    Update declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +29,6 @@ import { UserFormComponent } from './user-form/user-form.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    UserFormComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
   providers: [ShoppingListService],

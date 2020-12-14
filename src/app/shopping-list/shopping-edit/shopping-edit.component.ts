64c9c2aac556bb57dfa6e0dcd6cd82bd4eca79e4
RdotSilva <RyandotSilva@gmail.com>
2020-12-13 20:04:46 -0500

    Update onAddItem to use NgForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,29 +9,19 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
-  @ViewChild('categoryInput') categoryInputRef: ElementRef;
-  @ViewChild('priceInput') priceInputRef: ElementRef;
-  @ViewChild('requiredInput') requiredInputRef: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
-  onAddItem() {
-    const ingredientName = this.nameInputRef.nativeElement.value;
-    const ingredientAmount = this.amountInputRef.nativeElement.value;
-    const ingredientCategory = this.categoryInputRef.nativeElement.value;
-    const ingredientPrice = this.priceInputRef.nativeElement.value;
-    const ingredientRequired = this.requiredInputRef.nativeElement.value;
+  onAddItem(form: NgForm) {
+    const { name, amount, category, price, required } = form.value;
 
     const newIngredient = new Ingredient(
-      ingredientName,
-      ingredientAmount,
-      ingredientCategory,
-      ingredientPrice,
-      ingredientRequired
+      name,
+      amount,
+      category,
+      price,
+      required
     );
     this.shoppingListService.addIngredient(newIngredient);
   }

5b4a3aba90530bf783984d45925c05e99c35b233
RdotSilva <RyandotSilva@gmail.com>
2020-12-21 21:05:13 -0500

    Add onDeleteRecipe method

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -38,4 +38,8 @@ export class RecipeDetailComponent implements OnInit {
   onEditRecipe() {
     this.router.navigate(['edit'], { relativeTo: this.route });
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+  }
 }

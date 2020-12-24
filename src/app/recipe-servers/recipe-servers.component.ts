6b0db5951a99d1d6b0e2371fdc5f99fcf0820607
RdotSilva <RyandotSilva@gmail.com>
2020-12-23 20:38:23 -0500

    Add servers list property

---
+++ src/app/recipe-servers/recipe-servers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-servers',
+  templateUrl: './recipe-servers.component.html',
+  styleUrls: ['./recipe-servers.component.css'],
+})
+export class RecipeServersComponent implements OnInit {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017),
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

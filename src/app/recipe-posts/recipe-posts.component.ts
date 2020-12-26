272b28f1f6b11a6ce39c81a75b567365f1cfa2a8
RdotSilva <RyandotSilva@gmail.com>
2020-12-26 18:49:21 -0500

    Add RecipePostsComponent class

---
+++ src/app/recipe-posts/recipe-posts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-recipe-posts',
+  templateUrl: './recipe-posts.component.html',
+  styleUrls: ['./recipe-posts.component.css'],
+})
+export class RecipePostsComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}

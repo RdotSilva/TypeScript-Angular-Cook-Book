49ccdf8ae0e5aa165f7d80ae485d9c586b14a537
RdotSilva <RyandotSilva@gmail.com>
2020-12-30 20:56:14 -0500

    Update onClearPosts method

--- src/app/recipe-posts/recipe-posts.component.ts
+++ src/app/recipe-posts/recipe-posts.component.ts
@@ -29,7 +29,9 @@ export class RecipePostsComponent implements OnInit {
   }
 
   onClearPosts() {
-    // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
 
   private loadPosts() {

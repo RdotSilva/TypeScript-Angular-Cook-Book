763bb93d70a615b4941a7ba319dfd47777b7350d
RdotSilva <RyandotSilva@gmail.com>
2020-12-31 21:23:31 -0500

    Add catchError operator

--- src/app/recipe-posts/posts.service.ts
+++ src/app/recipe-posts/posts.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { config } from '../../../config';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -48,6 +48,9 @@ export class PostsService {
           }
         }
         return postsArray;
+      }),
+      catchError((errorRes) => {
+        return throwError(errorRes);
       })
     );
   }

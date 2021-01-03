70da51fc970cfdb8c6a7eb18eea1bf48ba2d513d
RdotSilva <RyandotSilva@gmail.com>
2021-01-02 21:08:46 -0500

    Update intercept to modify request

--- src/app/recipe-posts/auth-interceptor.service.ts
+++ src/app/recipe-posts/auth-interceptor.service.ts
@@ -7,6 +7,13 @@ import {
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log(`Request is on its way`);
-    return next.handle(req);
+    console.log(`Request URL: ${req.url}`);
+
+    let modifiedRequest = req.clone({
+      url: 'new-url',
+      headers: req.headers.append('Auth', 'xyz'),
+    });
+
+    return next.handle(modifiedRequest);
   }
 }

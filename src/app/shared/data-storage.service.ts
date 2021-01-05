4b68b7dd3e4162432a9d169dbeda9217294212f1
RdotSilva <RyandotSilva@gmail.com>
2021-01-04 20:58:49 -0500

    Add fetchRecipes method

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { config } from '../../../config';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -17,5 +18,11 @@ export class DataStorageService {
       .subscribe((response) => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>(`${config.url}`).subscribe((recipes) => {
+      this.recipeService.setRecipes(recipes);
+    });
   }
 }

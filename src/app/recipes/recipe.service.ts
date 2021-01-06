d93ed2c3516d08190d312f6a3b08d732e6e3ef4b
RdotSilva <RyandotSilva@gmail.com>
2021-01-05 21:05:39 -0500

    Move mock recipe data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,41 +2,14 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { mockRecipes } from './mock-recipes';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Test Recipe',
-      'This is only a test',
-      'https://static2.clutch.co/s3fs-public/logos/recipe_advertising_logo.png',
-      [
-        new Ingredient('Ground Beef', 1, 'Produce', 5, 'Yes'),
-        new Ingredient('Shells', 2, 'Produce', 2, 'Yes'),
-      ]
-    ),
-    new Recipe(
-      'Test Recipe Two',
-      'This is another test',
-      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
-      [
-        new Ingredient('Tomatoe', 1, 'Produce', 5, 'Yes'),
-        new Ingredient('Banana', 2, 'Produce', 2, 'Yes'),
-      ]
-    ),
-    new Recipe(
-      'Test Recipe Three',
-      'This is third test',
-      'https://pxhere.com/en/photo/1630317',
-      [
-        new Ingredient('Fish', 1, 'Produce', 5, 'Yes'),
-        new Ingredient('Onions', 2, 'Produce', 2, 'Yes'),
-      ]
-    ),
-  ];
+  private recipes: Recipe[] = mockRecipes;
 
   constructor(private shoppingListService: ShoppingListService) {}
 

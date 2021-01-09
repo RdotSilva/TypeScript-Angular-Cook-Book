a71e0e047ad1dd4452634df263ed507839bb0a2b
RdotSilva <RyandotSilva@gmail.com>
2021-01-08 21:46:59 -0500

    Add signUp method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -14,4 +14,15 @@ interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${config.key}`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    );
+  }
 }

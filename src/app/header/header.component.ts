51dff6374b60a193a8485d3b7e5f56e1bbb516a0
RdotSilva <RyandotSilva@gmail.com>
2021-01-12 17:35:15 -0500

    Add ngOnInit method

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit {
+  private userSub: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe();
+  }
 
   /**
    * Save data to Firebase database

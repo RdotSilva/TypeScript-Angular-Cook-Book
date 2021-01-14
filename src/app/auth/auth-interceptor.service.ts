ea1f393995068da47de16c6c96dd83ae15f01900
RdotSilva <RyandotSilva@gmail.com>
2021-01-14 18:44:00 -0500

    Update intercept to use operators

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,9 +1,11 @@
 import {
   HttpHandler,
   HttpInterceptor,
+  HttpParams,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { exhaustMap, take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -11,7 +13,15 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    this.authService.user.subscribe();
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        const modifiedReq = req.clone({
+          params: new HttpParams().set('auth', user.token),
+        });
+        return next.handle(modifiedReq);
+      })
+    );
     return next.handle(req);
   }
 }

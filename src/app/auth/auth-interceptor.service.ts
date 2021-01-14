92c8e37089b4edd91f79dbc33096cdc6cb42e73f
RdotSilva <RyandotSilva@gmail.com>
2021-01-14 18:39:44 -0500

    Add constructor

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -4,10 +4,14 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    this.authService.user.subscribe();
     return next.handle(req);
   }
 }

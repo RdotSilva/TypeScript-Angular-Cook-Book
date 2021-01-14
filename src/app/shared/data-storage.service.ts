22215a58a644112faffad0584988e0e228c3e308
RdotSilva <RyandotSilva@gmail.com>
2021-01-13 19:48:30 -0500

    Update fetchRecipes to use authService

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,11 +3,16 @@ import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { config } from '../../../config';
 import { Recipe } from '../recipes/recipe.model';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   /**
    * Make HTTP put request to Firebase server w/ recipe data
@@ -25,7 +30,11 @@ export class DataStorageService {
    * Make HTTP get request to Firebase server to fetch recipe data
    */
   fetchRecipes() {
-    return this.http.get<Recipe[]>(`${config.url}`).pipe(
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(`${config.url}`);
+      }),
       map((recipes) => {
         return recipes.map((recipe) => {
           return {

bbef3dd8f97f55341e3b155f81589e8eab7c4586
RdotSilva <RyandotSilva@gmail.com>
2021-01-15 17:41:51 -0500

    Add redirect on logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { config } from '../../../config';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
   kind: string;
@@ -19,7 +20,7 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signUp(email: string, password: string) {
     return this.http
@@ -69,6 +70,7 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 
   private handleAuthentication(

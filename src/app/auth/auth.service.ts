1bfc6aeb0fe2ca96eb29e5b81fd5be95db42b119
RdotSilva <RyandotSilva@gmail.com>
2021-01-16 18:56:20 -0500

    Add logic for expiration logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -93,6 +93,12 @@ export class AuthService {
 
     if (loadedUser.token) {
       this.user.next(loadedUser);
+
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+
+      this.autoLogout(expirationDuration);
     }
   }
 
@@ -128,6 +134,7 @@ export class AuthService {
     const user = new User(email, userId, token, expirationDate);
 
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 

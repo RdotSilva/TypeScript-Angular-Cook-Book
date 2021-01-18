54c910d96e2006f401e84ca8ebcda12175368a54
RdotSilva <RyandotSilva@gmail.com>
2021-01-17 19:41:13 -0500

    Add canActivate method

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -5,8 +5,20 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    router: RouterStateSnapshot
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return this.authService.user.pipe(
+      map((user) => {
+        return !!user;
+      })
+    );
+  }
 }
